b89bcd4094517f47636440e3cba65dbf445ad327
Mattia Rombi <mattyro96@gmail.com>
2022-04-05 23:05:31 +0200

    Added projects files

---
+++ data.lua
@@ -0,0 +1,4 @@
+data.raw["offshore-pump"]["offshore-pump"].adjacent_tile_collision_mask = nil
+data.raw["offshore-pump"]["offshore-pump"].adjacent_tile_collision_test = { "ground-tile", "water-tile" }
+data.raw["offshore-pump"]["offshore-pump"].flags = { "placeable-neutral", "player-creation" }
+data.raw["offshore-pump"]["offshore-pump"].placeable_position_visualization = nil
